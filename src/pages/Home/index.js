e9521dba1dd33f55ac842ac98e027382e35bb1f8
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-25 20:43:26 -0800

    started home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
+          alt=""
+        />
+        <strong>Shoes</strong>
+        <span>$150,50</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
