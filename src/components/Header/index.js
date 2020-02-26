e177f130c4973e96280319d1b989c65836f414ee
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-25 20:21:27 -0800

    added routes

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import logo from '../../assets/images/logo.svg';
+import { Container, Cart } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>My Cart</strong>
+          <span>3 Items</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
