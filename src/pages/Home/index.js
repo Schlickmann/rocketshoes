54fee8e5e2bf3960b1f2d3138aa346ef7dd1b9e4
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-25 21:38:00 -0800

    added function to format product price

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../utils/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/f_auto/q_auto/t_PDP_864_v1/87aa967b-de41-46bd-9e6d-8cd4aeb5910e/air-max-270-react-eng-shoe-KP30mK.jpg"
-          alt=""
-        />
-        <strong>Shoes</strong>
-        <span>$150,50</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={String(product.id)}>
+            <img src={product.image} alt="" />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADD TO CART</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
