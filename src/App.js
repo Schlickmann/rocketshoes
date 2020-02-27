fc830a933cb80301eb9722d721d6cd5b622c1a05
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-26 20:13:36 -0800

    added react-toastify to alert user when product run out of stock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
@@ -16,6 +17,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyles />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
