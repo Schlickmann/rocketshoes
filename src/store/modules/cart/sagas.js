63e55219c954fd460ac41e0892220ecb50fd5bb7
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-27 19:45:39 -0800

    added history to handle route navigation

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,6 +1,7 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
 
 import { Types } from './reducer';
@@ -35,6 +36,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
