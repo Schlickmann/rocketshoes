314af15a2a00b368fe827f160e76bb90e80e4efa
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-26 14:34:47 -0700

    fixed eslint error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rocketshoes-fake-api.herokuapp.com/',
+  baseURL: 'https://rocketshoes-fake-json-api.herokuapp.com/',
 });
 
 export default api;
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,7 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
-import history from '../../../services/history';
+// import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
 
 import { Types } from './reducer';
